cdbb7a9bd2bc2e0aa2a1e87735a707e3b7bacb1c
brianlan <brianlanbo@gmail.com>
2017-10-31 10:11:55 +0800

    added num_args checking for download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-UBT_VER=$1; shift;
-PACKAGE=$1; shift;
 ARCHIVE_DIR="/var/cache/apt/archives"
 TAR_PATH="archive.tar"
+
+if [ $# -ne 2 ]
+  then
+    echo "Invalid number of arguments. Usage: download.sh <ubuntu_version> <package_name>"
+    exit 1
+fi
+
+UBT_VER=$1; shift;
+PACKAGE=$1; shift;
 
 echo "start to build docker image"
 docker build -t tmp_deb_img --build-arg ubuntu_version=${UBT_VER} .
